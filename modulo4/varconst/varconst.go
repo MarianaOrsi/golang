48bf42d35a9e552f70d0c81c77dc4abe9beed1ae
Mariana Orsi <marianaorsi95@gmail.com>
2025-01-21 19:25:43 -0300

    Entendendo as variaveis e constante

---
+++ modulo4/varconst/varconst.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Variaveis
+	var nome string
+	nome = "Mariana"
+	fmt.Println(nome)
+
+	nome = "Orsi"
+	fmt.Println(nome)
+
+	var idade int
+	idade = 29
+	fmt.Println(idade)
+
+	var a, b int = 1, 2
+	fmt.Println(a, b)
+
+	var c = "Moraes"
+	fmt.Println(c)
+
+	fruta := "apple"
+	fmt.Println(fruta)
+
+	numero := 7
+	fmt.Println(numero)
+
+
+	// Constantes (const)
+	const idadeMari = 29
+	fmt.Println(idadeMari)
+}
